f3999cb4fb1a931ae8a30cf3f919b2ed24ba0a91
Samuel Dubuisson <sdubuisson08@gmail.com>
2024-04-23 13:08:08 +0200

    compute IDF and similarity scores

--- src/tree_matching/bipartite_graph.rs
+++ src/tree_matching/bipartite_graph.rs
@@ -1,12 +1,15 @@
-#[derive(Debug, PartialEq)]
+use super::indexer::Tokens;
+
+#[derive(PartialEq, Debug)]
 pub struct Edge {
     pub source: Node,
     pub target: Node,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Eq, Hash)]
 pub struct Node {
     pub name: String,
+    pub tokens : Tokens,
 }
 
 pub struct BipartiteGraph {
@@ -19,7 +22,33 @@ impl Clone for Node {
     fn clone(&self) -> Self {
         Node {
             name: self.name.clone(), // Cloning the inner String
+            tokens: self.tokens.clone(), // Cloning the inner Vec<String>
         }
+    }
+}
+
+impl Node {
+    pub fn new(name: &str) -> Node {
+        Node {
+            name: name.to_string(),
+            tokens: vec![],
+        }
+    }
+
+    pub fn add_token(&mut self, token: &str) {
+        self.tokens.push(token.to_string());
+    }
+
+    pub fn remove_token(&mut self, token: &str) {
+        self.tokens.retain(|t| t != token);
+    }
+
+    pub fn remove_all_tokens(&mut self) {
+        self.tokens.clear();
+    }
+
+    pub fn get_tokens(&self) -> &Vec<String> {
+        &self.tokens
     }
 }
 
---
+++ src/tree_matching/indexer.rs
@@ -0,0 +1,54 @@
+use std::collections::HashMap;
+
+use super::bipartite_graph;
+
+pub struct Indexer {
+    pub nodes: Vec<bipartite_graph::Node>,
+    pub token_map : HashMap<String, Vec<bipartite_graph::Node>>,
+}
+
+impl Indexer {
+    pub fn new() -> Indexer {
+        Indexer {
+            nodes: vec![],
+            token_map : HashMap::new(),
+        }
+    }
+
+    pub fn add_node(&mut self, node: bipartite_graph::Node) {
+        println!("Adding node {:?}", node);
+        self.nodes.push(node.clone());
+        for tk in &node.tokens {
+            self.token_map.entry(tk.clone()).or_insert(vec![]).push(node.clone());
+        }
+    }
+
+    pub fn compute_similarity_scores(&self, node: &bipartite_graph::Node) -> HashMap<&bipartite_graph::Node, f64> {
+        let mut neighbors: HashMap<&bipartite_graph::Node, f64> = HashMap::new();
+
+        println!("Node tokens: {:?}", node.tokens);
+        println!("Token map: {:?}", self.token_map);
+
+        // For each token contained in the token list of the node
+        for tk in &node.tokens {
+            // If the token is present in the token map
+            if let Some(nodes) = self.token_map.get(tk) {
+                // For each node in the list of nodes associated with the token
+                for n in nodes {
+                    // Add the idf of the token to the score of the node
+                    *neighbors.entry(&n).or_insert(0.0) += self.compute_idf(&tk);
+                }
+            }
+        }
+        neighbors
+    }
+            
+
+    pub fn compute_idf(&self, token: &str) -> f64 {
+        let count = self.token_map.get(token).map_or(0, |v| v.len());
+        // Take into account the case where count is 0 
+        ((self.nodes.len() as f64) / (count as f64)).ln()
+    }
+}
+
+pub type Tokens = Vec<String>;

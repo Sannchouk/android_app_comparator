e25ca288cebe86093013473c31472dc524650f71
Samuel Dubuisson <samuel.dubuisson.etu@univ-lille.fr>
2024-04-24 17:19:59 +0200

    metropolis algorithm

--- src/tree_matching/metropolis_algorithm.rs
+++ src/tree_matching/metropolis_algorithm.rs
@@ -14,12 +14,13 @@ pub struct MetropolisAlgorithm {
 impl MetropolisAlgorithm {
 
     pub fn new(graph: BipartiteGraph, beta: f64, gamma: f64, nb_iterations: usize) -> MetropolisAlgorithm {
+        let current_mapping = graph.edges.clone();
         MetropolisAlgorithm {
             graph,
             beta,
             gamma,
             nb_iterations,
-            current_mapping: vec![],
+            current_mapping: current_mapping,
         }
     }
 
@@ -27,6 +28,11 @@ impl MetropolisAlgorithm {
         &self.graph
     }
 
+    pub fn get_mapping(&self) -> &Mapping {
+        &self.current_mapping
+    }
+
+    /// Compute the cost of a given matching
     pub fn compute_cost(&self, edges: &Mapping) -> f64 {
         let mut cost:f64 = 0.0;
         for edge in edges {
@@ -38,6 +44,7 @@ impl MetropolisAlgorithm {
         cost
     }
 
+    /// Iters through a list of edges. At each step, it returns the edge with a probability of gamma
     pub fn select_edge_from(&self, edges: &Mapping) -> Option<Edge> {
         let mut rng = rand::thread_rng();
         for edge in edges {
@@ -48,41 +55,91 @@ impl MetropolisAlgorithm {
         None
     }
 
-    fn connected_edges(&self, edge: &Edge) -> Vec<Edge> {
+    /// Returns all the edges connected to a given edge
+    fn connected_edges(&self, edges: &Vec<Edge>, edge: &Edge) -> Vec<Edge> {
         let mut connected_edges = vec![];
-        for e in &self.graph.edges {
-            if e.source == edge.source || e.source == edge.target || e.target == edge.source || e.target == edge.target {
+        for e in edges {
+            if (e.source == edge.source) ^ (e.target == edge.target){
                 connected_edges.push(e.clone());
             }
         }
         connected_edges
     }
     
+    /// Removes a list of edges from a list of edges
     fn remove_edges(edges: &mut Vec<Edge>, edges_to_remove: &[Edge]) {
         edges.retain(|e| !edges_to_remove.contains(e));
     }
     
-    pub fn remove_all_adjacent_edges(&mut self, edge: &Edge) {
-        let connected_edges = self.connected_edges(edge);
-        Self::remove_edges(&mut self.graph.edges, &connected_edges);
+    /// Removes all the edges connected to a given edge from a list of edges
+    pub fn remove_all_adjacent_edges(&mut self, edges: &mut Mapping, edge: &Edge){
+        let connected_edges = self.connected_edges(edges, edge);
+        Self::remove_edges(edges, &connected_edges);
     }
     
-    pub fn select_new_matching(&mut self, edges: &mut Mapping) -> Mapping {
-        let mut new_matching = vec![];
+    /// Suggest a new mapping from the precendent one 
+    pub fn select_new_mapping(&mut self) -> Mapping {
+        let mut new_mapping = vec![];
         let mut remaining_edges = self.graph.edges.clone();
         remaining_edges.sort_by(|a, b| a.value.partial_cmp(&b.value).unwrap());
-        let to_keep = rand::thread_rng().gen_range(0..edges.len());
+        let to_keep = rand::thread_rng().gen_range(0..self.current_mapping.len());
+        println!("To keep: {:?}", to_keep);
         for _ in 0..to_keep {
-            let edge = remaining_edges.pop().unwrap();
-            new_matching.push(edge.clone());
-            self.remove_all_adjacent_edges(&edge);
+            println!("Remaining edges: {:?}", remaining_edges);
+            let edge = remaining_edges.pop();
+            if edge.is_none() {
+                break;
+            }
+            let edge = edge.unwrap();
+            new_mapping.push(edge.clone());
+            self.remove_all_adjacent_edges(&mut remaining_edges, &edge);
         }
+        println!("Wip mapping: {:?}", new_mapping);
         while !remaining_edges.is_empty() {
-            let edge = remaining_edges.pop().unwrap();
-            new_matching.push(edge.clone());
-            self.remove_all_adjacent_edges(&edge);
+            let edge = self.select_edge_from(&remaining_edges);
+            if edge.is_none() {
+                break;
+            }
+            let edge = edge.unwrap();
+            new_mapping.push(edge.clone());
+            self.remove_all_adjacent_edges(&mut remaining_edges, &edge);
         }
-        self.graph.edges = new_matching.clone();
-        new_matching
+        new_mapping
+    }
+
+    pub fn is_full_mapping(&self, mapping: &Mapping) -> bool {
+        let mut nodes = vec![];
+        for edge in mapping {
+            if nodes.contains(&edge.source) || nodes.contains(&edge.target) {
+                return false;
+            }
+            nodes.push(edge.source.clone());
+            nodes.push(edge.target.clone());
+        }
+        let mut res = true;
+        let mut group1 = self.graph.node_group_1.clone();
+        let mut group2 = self.graph.node_group_2.clone();
+        let mut wanted_nodes = &mut group1;
+        wanted_nodes.append(&mut group2);
+        for node in wanted_nodes {
+            if !nodes.contains(&node) {
+                res = false;
+            }
+        }
+        res
+    }
+
+    
+
+    pub fn run(&mut self) {
+        let mut current_cost = self.compute_cost(&self.current_mapping);
+        for _ in 0..self.nb_iterations {
+            let new_mapping = self.select_new_mapping();
+            let new_cost = self.compute_cost(&new_mapping);
+            if new_cost < current_cost && self.is_full_mapping(&new_mapping){
+                self.current_mapping = new_mapping;
+                current_cost = new_cost;
+            }
+        }
     }
 }
--- tests/metropolis_algorihm.rs
+++ tests/metropolis_algorihm.rs
@@ -24,6 +24,7 @@ mod tests {
         assert!(cost == expected_cost);
     }
 
+    #[test]
     fn test_select_edge_from() {
         let mut graph = BipartiteGraph::new_empty(); 
         let node1 = Node { name: "node1".to_string(), tokens: vec![] };
@@ -36,30 +37,47 @@ mod tests {
 
         graph.add_nodes(vec![node1.clone(), node2.clone()], 1);
         graph.add_nodes(vec![node3.clone()], 2);
-        graph.add_edges(edges.clone());
+        let _ = graph.add_edges(edges.clone());
 
-        let metropolis_algorithm = MetropolisAlgorithm::new(graph, 2.5, 0.8, 10);
+        let metropolis_algorithm = MetropolisAlgorithm::new(graph, 2.5, 1.0, 10);
         let edge = metropolis_algorithm.select_edge_from(&edges);
         assert!(edge.is_some());
     }
 
-    fn test_remove_all_adjacent_edges() {
-        let mut graph = BipartiteGraph::new_empty(); 
+    #[test]
+    fn test_algorithm() {
+        // GIVEN
         let node1 = Node { name: "node1".to_string(), tokens: vec![] };
         let node2 = Node { name: "node2".to_string(), tokens: vec![] };
         let node3 = Node { name: "node3".to_string(), tokens: vec![] };
+        let node4 = Node { name: "node4".to_string(), tokens: vec![] };
+        let edge1 = Edge { source: node1.clone(), target: node3.clone(), value: 1.0 };
+        let edge2 = Edge { source: node1.clone(), target: node4.clone(), value: 2.0 };
+        let edge3 = Edge { source: node2.clone(), target: node3.clone(), value: 4.0 };
+        let edge4 = Edge { source: node2.clone(), target: node4.clone(), value: 3.0 };
         let edges: Mapping = vec![
-            Edge { source: node2.clone(), target: node3.clone(), value: 2.0 },
-            Edge { source: node1.clone(), target: node3.clone(), value: 3.0 },
+            edge1.clone(), edge2.clone(), edge3.clone(), edge4.clone(),
         ];
-
+        let mut graph = BipartiteGraph::new_empty(); 
         graph.add_nodes(vec![node1.clone(), node2.clone()], 1);
         graph.add_nodes(vec![node3.clone()], 2);
-        graph.add_edges(edges.clone());
-
+        let _ = graph.add_edges(edges.clone());
         let mut metropolis_algorithm = MetropolisAlgorithm::new(graph, 2.5, 0.8, 10);
-        let edge = &edges[0];
-        metropolis_algorithm.remove_all_adjacent_edges(edge);
-        assert!(metropolis_algorithm.get_graph().edges.len() == 1);
+        let biggest_cost = metropolis_algorithm.compute_cost(&vec![
+            edge1.clone(), edge4.clone(),
+        ]);
+        let lowest_cost = metropolis_algorithm.compute_cost(&vec![
+            edge2.clone(), edge3.clone(),
+        ]);
+
+        // WHEN
+        let _ = metropolis_algorithm.run();
+
+        let mapping = metropolis_algorithm.get_mapping();
+        let final_cost = metropolis_algorithm.compute_cost(mapping);
+        println!("Final mapping: {:?}", metropolis_algorithm.get_mapping());
+        println!("Final cost: {}", final_cost);
+        assert!(mapping.len() == 2);
+        assert!(final_cost == lowest_cost);
     }
 }

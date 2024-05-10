15e5edf70a8cacb60c7e301c66be3e79f788c5ed
Dubuisson Samuel <sdubuisson08@gmail.com>
2024-05-10 17:33:57 +0200

    use of parameterized tests

--- src/test/java/IntegrationTest.java
+++ src/test/java/IntegrationTest.java
@@ -10,14 +10,18 @@ import matching.MetropolisAlgorithm;
 import matching.SimilarityScoresComputer;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import utils.AppConfigModifier;
 
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -31,27 +35,26 @@ public class IntegrationTest {
     }
 
 
-    @Test
-    void testApp() throws IOException {
+    @ParameterizedTest
+    @MethodSource({"tokenProperties"})
+    void testApp(Map<String, Boolean> tokenProperties) throws IOException {
+        //GIVEN
         Path apk1 = Paths.get("resources/lichess-apk");
         Path apk2 = Paths.get("resources/chesscom-apk");
-
         FileTree tree1 = FileTree.buildTree(apk1);
         FileTree tree2 = FileTree.buildTree(apk2);
-
         BipartiteGraph graph;
-        appConfigModifier.modifyConfigForTesting(Map.of("fileSize", false, "fileHash", false));
+        ConfigFactory.invalidateCaches();
+        appConfigModifier.modifyConfigForTesting(tokenProperties);
         graph = BipartiteGraph.buildFromTrees(tree1, tree2);
         List<Node> graph_nodes_1 = graph.getNodeGroup1();
         List<Node> graph_nodes_2 = graph.getNodeGroup2();
-
         Indexer indexer = new Indexer();
         indexer.addNodes(graph_nodes_1, 1);
         indexer.addNodes(graph_nodes_2, 2);
         var similarityScoresComputer = new SimilarityScoresComputer(indexer);
         var similarityScores = similarityScoresComputer.computeSimilarityScores();
         graph.buildEdgesFromNeighborhoods(similarityScores);
-
         MetropolisAlgorithm metropolisAlgorithm = new MetropolisAlgorithm(
                 graph,
                 2.5,
@@ -59,27 +62,28 @@ public class IntegrationTest {
                 10
         );
 
+        //WHEN
         metropolisAlgorithm.run();
-
         List<Edge> matching = metropolisAlgorithm.getMatching();
+
+        //THEN
+        assertTrue(graph_nodes_1.stream().anyMatch(node -> node.getTokens().size() == 1 + tokenProperties.values().stream().filter(b -> b).count()));
         assertTrue(matching.size() < graph_nodes_1.size());
         assertFalse(matching.isEmpty());
     }
 
-    @Test
-    void testAppWithTwoIdenticalApks() throws IOException {
+    @ParameterizedTest
+    @MethodSource({"tokenProperties"})
+    void testAppWithTwoIdenticalApks(Map<String, Boolean> tokenProperties) throws IOException {
+        //GIVEN
         Path apk1 = Paths.get("resources/lichess-apk");
-        Path apk2 = Paths.get("resources/lichess-apk");
-
         FileTree tree1 = FileTree.buildTree(apk1);
-        FileTree tree2 = FileTree.buildTree(apk2);
-
-        BipartiteGraph graph;
-        appConfigModifier.modifyConfigForTesting(Map.of("fileSize", false, "fileHash", false));
-        graph = BipartiteGraph.buildFromTrees(tree1, tree2);
+        FileTree tree2 = FileTree.buildTree(apk1);
+        ConfigFactory.invalidateCaches();
+        appConfigModifier.modifyConfigForTesting(tokenProperties);
+        BipartiteGraph graph = BipartiteGraph.buildFromTrees(tree1, tree2);
         List<Node> graph_nodes_1 = graph.getNodeGroup1();
         List<Node> graph_nodes_2 = graph.getNodeGroup2();
-
         Indexer indexer = new Indexer();
         indexer.addNodes(graph_nodes_1, 1);
         indexer.addNodes(graph_nodes_2, 2);
@@ -87,7 +91,6 @@ public class IntegrationTest {
         var similarityScores = similarityScoresComputer.computeSimilarityScores();
         graph.buildEdgesFromNeighborhoods(similarityScores);
         assertFalse(graph.getEdges().isEmpty());
-
         MetropolisAlgorithm metropolisAlgorithm = new MetropolisAlgorithm(
                 graph,
                 2.5,
@@ -95,26 +98,29 @@ public class IntegrationTest {
                 10
         );
 
+        //WHEN
         metropolisAlgorithm.run();
-
         List<Edge> matching = metropolisAlgorithm.getMatching();
+
+        //THEN
+        assertTrue(graph_nodes_1.stream().anyMatch(node -> node.getTokens().size() == 1 + tokenProperties.values().stream().filter(b -> b).count()));
         assertEquals(graph_nodes_1.size(), matching.size());
     }
 
-    @Test
-    void testAppWithTwoIdenticalApksWithDifferentVersions() throws IOException {
+    @ParameterizedTest
+    @MethodSource({"tokenProperties"})
+    void testAppWithTwoIdenticalApksWithDifferentVersions(Map<String, Boolean> tokenProperties) throws IOException {
+        //GIVEN
         Path apk1 = Paths.get("resources/lichess-apk");
         Path apk2 = Paths.get("resources/lichess-2021-apk");
-
         FileTree tree1 = FileTree.buildTree(apk1);
         FileTree tree2 = FileTree.buildTree(apk2);
-
         BipartiteGraph graph;
-        appConfigModifier.modifyConfigForTesting(Map.of("fileSize", false, "fileHash", false));
+        ConfigFactory.invalidateCaches();
+        appConfigModifier.modifyConfigForTesting(tokenProperties);
         graph = BipartiteGraph.buildFromTrees(tree1, tree2);
         List<Node> graph_nodes_1 = graph.getNodeGroup1();
         List<Node> graph_nodes_2 = graph.getNodeGroup2();
-
         Indexer indexer = new Indexer();
         indexer.addNodes(graph_nodes_1, 1);
         indexer.addNodes(graph_nodes_2, 2);
@@ -122,7 +128,6 @@ public class IntegrationTest {
         var similarityScores = similarityScoresComputer.computeSimilarityScores();
         graph.buildEdgesFromNeighborhoods(similarityScores);
         assertFalse(graph.getEdges().isEmpty());
-
         MetropolisAlgorithm metropolisAlgorithm = new MetropolisAlgorithm(
                 graph,
                 2.5,
@@ -130,55 +135,19 @@ public class IntegrationTest {
                 10
         );
 
+        //WHEN
         metropolisAlgorithm.run();
-
         List<Edge> matching = metropolisAlgorithm.getMatching();
+
+        //THEN
+        assertTrue(graph_nodes_1.stream().anyMatch(node -> node.getTokens().size() == 1 + tokenProperties.values().stream().filter(b -> b).count()));
         assertTrue(matching.size() > graph_nodes_1.size() / 2);
         assertTrue(matching.size() < graph_nodes_1.size());
     }
 
-    @Test
-    void testAppWithTwoIdenticalApksAndAllTokens() throws IOException {
-        Path apk1 = Paths.get("resources/lichess-apk");
-
-        FileTree tree1 = FileTree.buildTree(apk1);
-        FileTree tree2 = FileTree.buildTree(apk1);
-
-        BipartiteGraph graph;
-        ConfigFactory.invalidateCaches();
-        appConfigModifier.modifyConfigForTesting(Map.of("fileSize", true, "fileHash", true));
-        System.out.println(ConfigFactory.load().getBoolean("fileSize"));
-        System.setProperty("fileSize", "true");
-        System.setProperty("fileHash", "true");
-        graph = BipartiteGraph.buildFromTrees(tree1, tree2);
-        List<Node> graph_nodes_1 = graph.getNodeGroup1();
-        List<Node> graph_nodes_2 = graph.getNodeGroup2();
-
-        Indexer indexer = new Indexer();
-        indexer.addNodes(graph_nodes_1, 1);
-        indexer.addNodes(graph_nodes_2, 2);
-        var similarityScoresComputer = new SimilarityScoresComputer(indexer);
-        var similarityScores = similarityScoresComputer.computeSimilarityScores();
-        graph.buildEdgesFromNeighborhoods(similarityScores);
-        assertFalse(graph.getEdges().isEmpty());
-
-        MetropolisAlgorithm metropolisAlgorithm = new MetropolisAlgorithm(
-                graph,
-                2.5,
-                0.8,
-                10
-        );
-
-        metropolisAlgorithm.run();
-        System.out.println(ConfigFactory.load().getBoolean("fileSize"));
-
-        List<Edge> matching = metropolisAlgorithm.getMatching();
-        assertTrue(graph_nodes_1.stream().anyMatch(node -> node.getTokens().size() == 3));
-        assertEquals(graph_nodes_1.size(), matching.size(), "All nodes should be matched but the following one are not : " +
-                " " + graph_nodes_1.stream().filter(node -> matching.stream().noneMatch(edge -> edge.getSource().equals(node))).collect(Collectors.toList()) +
-                "\n" + graph_nodes_2.stream().filter(node -> matching.stream().noneMatch(edge -> edge.getTarget().equals(node))).collect(Collectors.toList())
-        );
+    private static Stream<Map<String, Boolean>> tokenProperties() {
+        Map<String, Boolean> noOtherTokens = Map.of("fileSize", false, "fileHash", false);
+        Map<String, Boolean> allTokens = Map.of("fileSize", true, "fileHash", true);
+        return Stream.of(noOtherTokens, allTokens);
     }
-
-
 }

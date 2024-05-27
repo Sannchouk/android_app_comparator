762d9d621fd96fb30c05898ebf9612e6dbc400c7
samuel.dubuisson <samuel.dubuisson.etu@univ-lille.fr>
2024-05-27 14:23:40 +0200

    extension distance

---
+++ src/main/java/matching/computers/extensions/ExtensionDistanceComputer.java
@@ -0,0 +1,8 @@
+package matching.computers.extensions;
+
+public class ExtensionDistanceComputer {
+
+    public int computeDistance(String extension1, String extension2) {
+        return (extension1 != null && extension1.equals(extension2)) ? 0 : 1;
+    }
+}
